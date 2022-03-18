9da21de1b62c8caed4d43bbd49d3177d22ceaada
akelleci <B>
2022-03-18 20:14:12 +0100

    minor change

--- eventify/src/main/java/io/github/alikelleci/eventify/messaging/commandhandling/CommandBus.java
+++ eventify/src/main/java/io/github/alikelleci/eventify/messaging/commandhandling/CommandBus.java
@@ -2,25 +2,37 @@ package io.github.alikelleci.eventify.messaging.commandhandling;
 
 import io.github.alikelleci.eventify.constants.Handlers;
 import io.github.alikelleci.eventify.constants.Topics;
+import io.github.alikelleci.eventify.messaging.Message;
+import io.github.alikelleci.eventify.messaging.Metadata;
 import io.github.alikelleci.eventify.messaging.eventhandling.Event;
 import io.github.alikelleci.eventify.support.serializer.CustomSerdes;
 import io.github.alikelleci.eventify.util.CommonUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Produced;
 
+import java.time.Instant;
+import java.util.UUID;
+
+@Slf4j
 public class CommandBus {
+
+  private Producer<String, Message> producer;
+
 
   public void subscribe() {
     StreamsBuilder builder = new StreamsBuilder();
 
     /*
-    *************************************************************************************
-    * Command Handling
-    *************************************************************************************
-    */
+     *************************************************************************************
+     * Command Handling
+     *************************************************************************************
+     */
 
     if (!Handlers.COMMAND_HANDLERS.isEmpty()) {
 
@@ -80,6 +92,43 @@ public class CommandBus {
 
 
   public void dispatch(Command command) {
+    //validatePayload(payload);
 
+    String aggregateId = command.getAggregateId();
+    long timestamp = command.getTimestamp().toEpochMilli();
+    Object payload = command.getPayload();
+    String topic = CommonUtils.getTopicInfo(payload).value();
+
+    ProducerRecord<String, Message> record = new ProducerRecord<>(topic, null, timestamp, aggregateId, command);
+
+    log.debug("Sending command: {} ({})", payload.getClass().getSimpleName(), aggregateId);
+    producer.send(record);
+  }
+
+  public void dispatch(Object payload, Metadata metadata) {
+    if (metadata == null) {
+      metadata = Metadata.builder().build();
+    }
+
+    String aggregateId = CommonUtils.getAggregateId(payload);
+    String messageId = CommonUtils.createMessageId(aggregateId);
+    Instant timestamp = Instant.now();
+    String correlationId = UUID.randomUUID().toString();
+
+    Command command = Command.builder()
+        .aggregateId(aggregateId)
+        .id(messageId)
+        .timestamp(timestamp)
+        .payload(payload)
+        .metadata(metadata.filter().toBuilder()
+            .entry(Metadata.CORRELATION_ID, correlationId)
+            .build())
+        .build();
+
+    dispatch(command);
+  }
+
+  public void dispatch(Object payload) {
+    dispatch(payload, null);
   }
 }
