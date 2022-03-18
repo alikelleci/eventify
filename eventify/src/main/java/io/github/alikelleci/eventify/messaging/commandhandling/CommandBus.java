a47bd1e3013ce57da4ca23034a9a33ee14e31b38
akelleci <B>
2022-03-18 20:30:10 +0100

    minor change

--- eventify/src/main/java/io/github/alikelleci/eventify/messaging/commandhandling/CommandBus.java
+++ eventify/src/main/java/io/github/alikelleci/eventify/messaging/commandhandling/CommandBus.java
@@ -1,31 +1,60 @@
 package io.github.alikelleci.eventify.messaging.commandhandling;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import io.github.alikelleci.eventify.constants.Handlers;
 import io.github.alikelleci.eventify.constants.Topics;
 import io.github.alikelleci.eventify.messaging.Message;
 import io.github.alikelleci.eventify.messaging.Metadata;
 import io.github.alikelleci.eventify.messaging.eventhandling.Event;
 import io.github.alikelleci.eventify.support.serializer.CustomSerdes;
+import io.github.alikelleci.eventify.support.serializer.JsonSerializer;
 import io.github.alikelleci.eventify.util.CommonUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Produced;
 
+import java.time.Duration;
 import java.time.Instant;
+import java.util.Properties;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 public class CommandBus {
 
+  private Properties producerConfig;
   private Producer<String, Message> producer;
 
+  private Properties streamsConfig;
+  private KafkaStreams kafkaStreams;
 
-  public void subscribe() {
+  private Cache<String, CompletableFuture<Object>> cache;
+
+  public CommandBus(Properties producerConfig, Properties streamsConfig) {
+    this.producerConfig = producerConfig;
+    setAdditionalProducerConfigs(this.producerConfig);
+
+    this.producer = new KafkaProducer<>(this.producerConfig,
+        new StringSerializer(),
+        new JsonSerializer<>());
+
+    this.cache = Caffeine.newBuilder()
+        .expireAfterWrite(Duration.ofMinutes(5))
+        .build();
+  }
+
+  protected Topology topology() {
     StreamsBuilder builder = new StreamsBuilder();
 
     /*
@@ -88,6 +117,13 @@ public class CommandBus {
     KStream<String, Command> results = builder.stream(Topics.RESULTS, Consumed.with(Serdes.String(), CustomSerdes.Json(Command.class)))
         .filter((key, command) -> key != null)
         .filter((key, command) -> command != null);
+
+
+    return builder.build();
+  }
+
+  public void subscribe() {
+    this.kafkaStreams = new KafkaStreams(topology(), this.streamsConfig);
   }
 
 
@@ -130,5 +166,19 @@ public class CommandBus {
 
   public void dispatch(Object payload) {
     dispatch(payload, null);
+  }
+
+  public void setAdditionalProducerConfigs(Properties producerConfig) {
+    producerConfig.putIfAbsent(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+    producerConfig.putIfAbsent(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+    producerConfig.putIfAbsent(ProducerConfig.ACKS_CONFIG, "all");
+    producerConfig.putIfAbsent(ProducerConfig.RETRIES_CONFIG, Integer.MAX_VALUE);
+    producerConfig.putIfAbsent(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
+
+//    ArrayList<String> interceptors = new ArrayList<>();
+//    interceptors.add(CommonProducerInterceptor.class.getName());
+//    interceptors.add(TracingProducerInterceptor.class.getName());
+//
+//    this.producerConfig.putIfAbsent(ProducerConfig.INTERCEPTOR_CLASSES_CONFIG, interceptors);
   }
 }
