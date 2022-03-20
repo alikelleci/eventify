d0f73b52aaf8faedf778bb58d3c653b0a0fe92ba
akelleci <B>
2022-03-20 14:17:29 +0100

    minor change

---
+++ eventify/src/test/java/io/github/alikelleci/eventify/experimental/Test.java
@@ -0,0 +1,41 @@
+package io.github.alikelleci.eventify.experimental;
+
+import io.github.alikelleci.eventify.common.annotations.AggregateId;
+import io.github.alikelleci.eventify.experimental.messages.Command;
+import lombok.Builder;
+import lombok.Value;
+
+import java.time.Instant;
+
+public class Test {
+
+  public static void main(String[] args) {
+
+    Customer customer = Customer.builder()
+        .id("cust-1")
+        .name("Henk")
+        .build();
+
+
+    Command command = new Command(Instant.ofEpochMilli(1584709244000L), customer, null);
+    System.out.println(command);
+    System.out.println(command.getId());
+    System.out.println(command.getTimestamp());
+    System.out.println(command.getPayload());
+    System.out.println(command.getMetadata());
+    System.out.println(command.getAggregateId());
+
+    new Command(null, null, null);
+    new Command(null, null);
+    new Command(null);
+  }
+
+  @Value
+  @Builder
+  static class Customer {
+    @AggregateId
+    private String id;
+    private String name;
+
+  }
+}
---
+++ eventify/src/test/java/io/github/alikelleci/eventify/experimental/messages/Event.java
@@ -0,0 +1,46 @@
+package io.github.alikelleci.eventify.experimental.messages;
+
+import io.github.alikelleci.eventify.common.annotations.AggregateId;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.springframework.util.ReflectionUtils;
+
+import java.time.Instant;
+import java.util.Optional;
+
+@Getter
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class Event extends Message {
+  private String aggregateId;
+
+  protected Event(String id, Instant timestamp, Object payload, Metadata metadata) {
+    super(id, timestamp, payload, metadata);
+    this.aggregateId = createAggregatieId(payload);
+  }
+
+  public Event(Instant timestamp, Object payload, Metadata metadata) {
+    this(null, timestamp, payload, metadata);
+  }
+
+  public Event(Object payload, Metadata metadata) {
+    this(null, null, payload, metadata);
+  }
+
+  public Event(Object payload) {
+    this(null, null, payload, null);
+  }
+
+  private String createAggregatieId(Object payload) {
+    return Optional.ofNullable(payload).flatMap(p -> FieldUtils.getFieldsListWithAnnotation(payload.getClass(), AggregateId.class).stream()
+        .filter(field -> field.getType() == String.class)
+        .findFirst()
+        .map(field -> {
+          field.setAccessible(true);
+          return (String) ReflectionUtils.getField(field, payload);
+        }))
+        .orElse(null);
+  }
+}
