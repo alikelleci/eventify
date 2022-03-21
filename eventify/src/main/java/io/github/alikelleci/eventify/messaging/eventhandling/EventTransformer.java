0a3aa569816add5474a7533f57f090bff98336ff
akelleci <B>
2022-03-21 02:25:52 +0100

    minor change

--- eventify/src/main/java/io/github/alikelleci/eventify/messaging/eventhandling/EventTransformer.java
+++ eventify/src/main/java/io/github/alikelleci/eventify/messaging/eventhandling/EventTransformer.java
@@ -1,26 +1,17 @@
 package io.github.alikelleci.eventify.messaging.eventhandling;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.github.alikelleci.eventify.Config;
 import io.github.alikelleci.eventify.messaging.eventsourcing.EventSourcingHandler;
 import io.github.alikelleci.eventify.messaging.eventsourcing.Repository;
-import io.github.alikelleci.eventify.messaging.upcasting.Upcaster;
-import io.github.alikelleci.eventify.messaging.upcasting.annotations.Upcast;
-import io.github.alikelleci.eventify.util.JacksonUtils;
+import io.github.alikelleci.eventify.messaging.upcasting.UpcasterUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.kafka.streams.kstream.ValueTransformerWithKey;
 import org.apache.kafka.streams.processor.ProcessorContext;
 
 import java.util.Collection;
 import java.util.Comparator;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.concurrent.atomic.AtomicInteger;
-
-import static io.github.alikelleci.eventify.messaging.Metadata.REVISION;
 
 @Slf4j
 public class EventTransformer implements ValueTransformerWithKey<String, JsonNode, Event> {
@@ -40,53 +31,20 @@ public class EventTransformer implements ValueTransformerWithKey<String, JsonNod
 
   @Override
   public Event transform(String key, JsonNode jsonNode) {
-    String className = Optional.ofNullable(jsonNode)
-        .map(node -> node.get("payload"))
-        .map(node -> node.get("@class"))
-        .map(JsonNode::textValue)
-        .orElse(null);
+    Event event = UpcasterUtil.upcast(config, jsonNode);
 
-    if (StringUtils.isBlank(className)) {
-      return null;
+    Collection<EventHandler> handlers = config.handlers().eventHandlers().get(event.getPayload().getClass());
+    if (CollectionUtils.isNotEmpty(handlers)) {
+      handlers.stream()
+          .sorted(Comparator.comparingInt(EventHandler::getPriority).reversed())
+          .forEach(handler ->
+              handler.apply(event));
     }
 
-
-    Event event;
-
-    Collection<Upcaster> upcasters = config.handlers().upcasters().get(className);
-    if (CollectionUtils.isEmpty(upcasters)) {
-      event = JacksonUtils.enhancedObjectMapper().convertValue(jsonNode, Event.class);
+    EventSourcingHandler eventSourcingHandler = config.handlers().eventSourcingHandlers().get(event.getPayload().getClass());
+    if (eventSourcingHandler != null) {
+      repository.saveEvent(event);
     }
-
-    AtomicInteger revision = Optional.ofNullable(jsonNode.get("metadata"))
-        .map(m -> m.get(REVISION))
-        .map(JsonNode::intValue)
-        .map(AtomicInteger::new)
-        .orElse(new AtomicInteger(1));
-
-    upcasters.stream()
-        .sorted(Comparator.comparingInt(handler -> handler.getMethod().getAnnotation(Upcast.class).revision()))
-        .filter(handler -> handler.getMethod().getAnnotation(Upcast.class).revision() == revision.get())
-        .map(handler -> handler.apply(jsonNode.get("payload")))
-        .forEach(result -> {
-          ((ObjectNode) jsonNode.get("metadata")).put(REVISION, revision.incrementAndGet());
-        });
-
-    System.out.println(jsonNode);
-    event = JacksonUtils.enhancedObjectMapper().convertValue(jsonNode, Event.class);
-
-//    Collection<EventHandler> handlers = config.handlers().eventHandlers().get(event.getPayload().getClass());
-//    if (CollectionUtils.isNotEmpty(handlers)) {
-//      handlers.stream()
-//          .sorted(Comparator.comparingInt(EventHandler::getPriority).reversed())
-//          .forEach(handler ->
-//              handler.apply(event));
-//    }
-//
-//    EventSourcingHandler eventSourcingHandler = config.handlers().eventSourcingHandlers().get(event.getPayload().getClass());
-//    if (eventSourcingHandler != null) {
-//      repository.saveEvent(event);
-//    }
 
     return event;
   }
