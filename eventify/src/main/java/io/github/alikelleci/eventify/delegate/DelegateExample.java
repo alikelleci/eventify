8242ed9d5f5ccac635d0c1051d4561fc262df090
akelleci <B>
2022-03-22 02:13:35 +0100

    minor change

---
+++ eventify/src/main/java/io/github/alikelleci/eventify/delegate/DelegateExample.java
@@ -0,0 +1,40 @@
+package io.github.alikelleci.eventify.delegate;
+
+import io.github.alikelleci.eventify.messaging.eventhandling.EventHandler;
+import io.github.alikelleci.eventify.messaging.eventhandling.annotations.HandleEvent;
+import io.github.alikelleci.eventify.util.HandlerUtils;
+import lombok.experimental.Delegate;
+import org.apache.commons.collections4.MultiValuedMap;
+
+import java.lang.reflect.Method;
+import java.util.List;
+
+public class DelegateExample implements MultiValuedMap<Class<?>, EventHandler> {
+
+  @Delegate(excludes = ExcludedMethods.class)
+  private MultiValuedMap<Class<?>, EventHandler> map;
+
+  @Override
+  public boolean put(Class<?> type, EventHandler handler) {
+    List<Method> eventHandlerMethods = HandlerUtils.findMethodsWithAnnotation(handler.getClass(), HandleEvent.class);
+    eventHandlerMethods
+        .forEach(method -> addEventHandler(handler, method));
+
+    return true;
+  }
+
+  private void addEventHandler(Object listener, Method method) {
+    if (method.getParameterCount() == 1 || method.getParameterCount() == 2) {
+      Class<?> type = method.getParameters()[0].getType();
+      this.put(type, new EventHandler(listener, method));
+    }
+  }
+
+  /**
+   * Excluded methods that Lombok will not implement, we will implement/override these methods.
+   */
+  private abstract class ExcludedMethods {
+
+    public abstract boolean put(Class<?> type, EventHandler handler);
+  }
+}
