cebb13dfc62b66b983a5f78f0474a57349342726
Tuna <B>
2022-03-22 20:33:02 +0100

    minor changes

--- eventify/src/main/java/io/github/alikelleci/eventify/messaging/Message.java
+++ eventify/src/main/java/io/github/alikelleci/eventify/messaging/Message.java
@@ -1,20 +1,24 @@
 package io.github.alikelleci.eventify.messaging;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.github.f4b6a3.ulid.UlidCreator;
+import io.github.alikelleci.eventify.common.annotations.AggregateId;
 import io.github.alikelleci.eventify.common.annotations.TopicInfo;
-import lombok.EqualsAndHashCode;
+import lombok.Data;
 import lombok.Getter;
 import lombok.ToString;
+import lombok.Value;
+import org.apache.commons.lang3.reflect.FieldUtils;
 import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.util.ReflectionUtils;
 
 import java.beans.Transient;
 import java.time.Instant;
 import java.util.Optional;
 
 @Getter
-@ToString()
-@EqualsAndHashCode()
-public abstract class Message {
+@ToString
+public class Message {
   private String id;
   private Instant timestamp;
   private String type;
@@ -22,26 +26,28 @@ public abstract class Message {
   private Object payload;
   private Metadata metadata;
 
-  protected Message() {
-  }
-
-  protected Message(String id, Instant timestamp, Object payload, Metadata metadata) {
-    this.id = id;
+  protected Message(Instant timestamp, Object payload, Metadata metadata) {
+    this.id = Optional.ofNullable(payload)
+        .flatMap(p -> FieldUtils.getFieldsListWithAnnotation(payload.getClass(), AggregateId.class).stream()
+        .filter(field -> field.getType() == String.class)
+        .findFirst()
+        .map(field -> {
+          field.setAccessible(true);
+          return (String) ReflectionUtils.getField(field, payload);
+        }))
+        .map(s -> s + "@" + UlidCreator.getMonotonicUlid().toString())
+        .orElse(null);
 
     this.timestamp = Optional.ofNullable(timestamp)
-        .orElse(Instant.now());
+        .orElse(Instant.now());;
 
     this.type = Optional.ofNullable(payload)
         .map(p -> p.getClass().getSimpleName())
         .orElse(null);
 
     this.payload = payload;
-    this.metadata = Optional.ofNullable(metadata)
-        .orElse(new Metadata());
-  }
 
-  public Metadata getMetadata() {
-    return Optional.ofNullable(this.metadata)
+    this.metadata = Optional.ofNullable(metadata)
         .map(m -> m
             .add(Metadata.ID, this.id)
             .add(Metadata.TIMESTAMP, this.timestamp.toString()))
