6e604508eb8a632872d22ff262d9d3ec00811db6
a.kelleci <a.kelleci@portbase.com>
2023-01-24 10:33:44 +0100

    minor changes

--- eventify/src/main/java/io/github/alikelleci/eventify/util/StateStoreNameResolver.java
+++ eventify/src/main/java/io/github/alikelleci/eventify/util/StateStoreNameResolver.java
@@ -1,15 +1,12 @@
 package io.github.alikelleci.eventify.util;
 
-import lombok.experimental.UtilityClass;
-
-@UtilityClass
 public class StateStoreNameResolver {
 
-  public String resolveEventStoreName(Class<?> aggregateType) {
+  public static String resolveEventStoreName(Class<?> aggregateType) {
     return aggregateType.getSimpleName() + "-event-store";
   }
 
-  public String resolveSnapshotStoreName(Class<?> aggregateType) {
+  public static String resolveSnapshotStoreName(Class<?> aggregateType) {
     return aggregateType.getSimpleName() + "-snapshot-store";
   }
 }
