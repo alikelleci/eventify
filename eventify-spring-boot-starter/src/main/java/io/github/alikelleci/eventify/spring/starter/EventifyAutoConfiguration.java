97a002893f3d721306aae79591571f621c49275a
a.kelleci <a.kelleci@portbase.com>
2023-07-05 01:51:17 +0200

    added support for spring boot 3

--- eventify-spring-boot-starter/src/main/java/io/github/alikelleci/eventify/spring/starter/EventifyAutoConfiguration.java
+++ eventify-spring-boot-starter/src/main/java/io/github/alikelleci/eventify/spring/starter/EventifyAutoConfiguration.java
@@ -3,19 +3,19 @@ package io.github.alikelleci.eventify.spring.starter;
 import io.github.alikelleci.eventify.Eventify;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
 
 import java.util.List;
 import java.util.Map;
 
 @Slf4j
-@Configuration
+@AutoConfiguration
 @ConditionalOnBean(Eventify.class)
 @EnableConfigurationProperties(EventifyProperties.class)
 public class EventifyAutoConfiguration {
