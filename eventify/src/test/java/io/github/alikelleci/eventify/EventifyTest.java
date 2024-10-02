c4a16b19e2eb1b0655bc8a730ea3ff939945c807
a.kelleci <a.kelleci@portbase.com>
2024-10-02 20:35:22 +0200

    minor changes

--- eventify/src/test/java/io/github/alikelleci/eventify/EventifyTest.java
+++ eventify/src/test/java/io/github/alikelleci/eventify/EventifyTest.java
@@ -96,38 +96,42 @@ class EventifyTest {
 
     @Test
     void commandShouldSucceed() {
-      Command command = buildCreateCustomerCommand("cust-1", "John", "Doe", 100);
-      commandsTopic.pipeInput(command.getAggregateId(), command);
+      List<Command> commands = List.of(
+          buildCreateCustomerCommand("cust-1", "John", "Doe", 100)
+      );
+      commands.forEach(command -> commandsTopic.pipeInput(command.getAggregateId(), command));
 
       List<Command> commandResults = commandResultsTopic.readValuesToList();
-      assertThat(commandResults.size(), is(1));
-      commandResults.forEach(commandResult -> assertCommandResult(command, commandResult, true));
+      assertThat(commandResults.size(), is(commands.size()));
+
+      assertCommandResult(commands.get(0), commandResults.get(0), true);
 
       List<Event> events = eventsTopic.readValuesToList();
       assertThat(events.size(), is(1));
-      events.forEach(event -> assertEvent(command, event));
+
+      assertEvent(commands.get(0), events.get(0), CustomerCreated.class);
+      assertThat(((CustomerCreated) events.get(0).getPayload()).getId(), is(((CreateCustomer) commands.get(0).getPayload()).getId()));
+      assertThat(((CustomerCreated) events.get(0).getPayload()).getFirstName(), is(((CreateCustomer) commands.get(0).getPayload()).getFirstName()));
+      assertThat(((CustomerCreated) events.get(0).getPayload()).getLastName(), is(((CreateCustomer) commands.get(0).getPayload()).getLastName()));
+      assertThat(((CustomerCreated) events.get(0).getPayload()).getCredits(), is(((CreateCustomer) commands.get(0).getPayload()).getCredits()));
+      assertThat(((CustomerCreated) events.get(0).getPayload()).getBirthday(), is(((CreateCustomer) commands.get(0).getPayload()).getBirthday()));
 
       List<Event> eventsInStore = readEventsFromStore();
       assertThat(eventsInStore.size(), is(1));
-      eventsInStore.forEach(event -> assertEvent(command, event));
       assertThat(eventsInStore, containsInRelativeOrder(events.toArray(new Event[0])));
-
-      assertEvent(command, events.get(0), CustomerCreated.class);
-      assertThat(((CustomerCreated) events.get(0).getPayload()).getId(), is(((CreateCustomer) command.getPayload()).getId()));
-      assertThat(((CustomerCreated) events.get(0).getPayload()).getFirstName(), is(((CreateCustomer) command.getPayload()).getFirstName()));
-      assertThat(((CustomerCreated) events.get(0).getPayload()).getLastName(), is(((CreateCustomer) command.getPayload()).getLastName()));
-      assertThat(((CustomerCreated) events.get(0).getPayload()).getCredits(), is(((CreateCustomer) command.getPayload()).getCredits()));
-      assertThat(((CustomerCreated) events.get(0).getPayload()).getBirthday(), is(((CreateCustomer) command.getPayload()).getBirthday()));
     }
 
     @Test
     void commandShouldFail() {
-      Command command = buildAddCreditsCommand("cust-1", 100);
-      commandsTopic.pipeInput(command.getAggregateId(), command);
+      List<Command> commands = List.of(
+          buildAddCreditsCommand("cust-1", 100)
+      );
+      commands.forEach(command -> commandsTopic.pipeInput(command.getAggregateId(), command));
 
       List<Command> commandResults = commandResultsTopic.readValuesToList();
-      assertThat(commandResults.size(), is(1));
-      commandResults.forEach(commandResult -> assertCommandResult(command, commandResult, false));
+      assertThat(commandResults.size(), is(commands.size()));
+
+      assertCommandResult(commands.get(0), commandResults.get(0), false);
 
       List<Event> events = eventsTopic.readValuesToList();
       assertThat(events.size(), is(0));
@@ -153,9 +157,7 @@ class EventifyTest {
           buildIssueCreditsCommand("cust-1", 200), // should fail
           buildIssueCreditsCommand("cust-1", 2)
       );
-
-      commands.forEach(command ->
-          commandsTopic.pipeInput(command.getAggregateId(), command));
+      commands.forEach(command -> commandsTopic.pipeInput(command.getAggregateId(), command));
 
       List<Command> commandResults = commandResultsTopic.readValuesToList();
       assertThat(commandResults.size(), is(commands.size()));
@@ -172,10 +174,6 @@ class EventifyTest {
       List<Event> events = eventsTopic.readValuesToList();
       assertThat(events.size(), is(6));
 
-      List<Event> eventsInStore = readEventsFromStore();
-      assertThat(eventsInStore.size(), is(6));
-      assertThat(eventsInStore, containsInRelativeOrder(events.toArray(new Event[0])));
-
       assertEvent(commands.get(0), events.get(0), CustomerCreated.class);
       assertEvent(commands.get(1), events.get(1), CreditsAdded.class);
       assertEvent(commands.get(2), events.get(2), CreditsAdded.class);
@@ -183,8 +181,13 @@ class EventifyTest {
       assertEvent(commands.get(4), events.get(4), CreditsAdded.class);
       assertEvent(commands.get(7), events.get(5), CreditsIssued.class);
 
+      List<Event> eventsInStore = readEventsFromStore();
+      assertThat(eventsInStore.size(), is(6));
+      assertThat(eventsInStore, containsInRelativeOrder(events.toArray(new Event[0])));
+
       Aggregate snapshot = snapshotStore.get(events.get(4).getAggregateId());
       assertThat(snapshot, is(notNullValue()));
+
       assertSnapshot(events.get(4), snapshot, Customer.class, 5);
       assertThat(((Customer) snapshot.getPayload()).getId(), is("cust-1"));
       assertThat(((Customer) snapshot.getPayload()).getFirstName(), is("John"));
