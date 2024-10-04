54447935cde4d50c436295fd128e538d2506ab66
a.kelleci <a.kelleci@portbase.com>
2024-10-04 15:37:57 +0200

    minor changes

--- eventify/src/main/java/io/github/alikelleci/eventify/support/serializer/custom/InstantDeserializer.java
+++ eventify/src/main/java/io/github/alikelleci/eventify/support/serializer/custom/InstantDeserializer.java
@@ -1,6 +1,5 @@
 package io.github.alikelleci.eventify.support.serializer.custom;
 
-import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonNode;
