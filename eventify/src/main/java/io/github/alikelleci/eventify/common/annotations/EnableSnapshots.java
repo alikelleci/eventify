f8963abc2568850edf242d46ee283def201ce629
a.kelleci <a.kelleci@portbase.com>
2024-10-07 11:50:03 +0200

    minor changes

--- eventify/src/main/java/io/github/alikelleci/eventify/common/annotations/EnableSnapshots.java
+++ eventify/src/main/java/io/github/alikelleci/eventify/common/annotations/EnableSnapshots.java
@@ -8,6 +8,6 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface EnableSnapshots {
-  int threshold() default 100;
+  int threshold() default 500;
   boolean deleteEvents() default false;
 }
