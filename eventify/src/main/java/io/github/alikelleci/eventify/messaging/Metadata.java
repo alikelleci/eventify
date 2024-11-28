9dae7bce3ebc2a0264c38c7389340b3aa6d7303e
a.kelleci <a.kelleci@portbase.com>
2024-11-28 16:03:00 +0100

    minor changes

--- eventify/src/main/java/io/github/alikelleci/eventify/messaging/Metadata.java
+++ eventify/src/main/java/io/github/alikelleci/eventify/messaging/Metadata.java
@@ -1,32 +1,55 @@
 package io.github.alikelleci.eventify.messaging;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Singular;
 import lombok.Value;
-import lombok.experimental.Delegate;
 
 import java.beans.Transient;
 import java.util.Collections;
 import java.util.Map;
 
 @Value
-public class Metadata implements Map<String, String> {
+@Builder(toBuilder = true)
+public class Metadata  {
   public static final String CORRELATION_ID = "$correlationId";
   public static final String REPLY_TO = "$replyTo";
   public static final String RESULT = "$result";
   public static final String CAUSE = "$cause";
 
-  @Delegate
+  @Singular
   Map<String, String> entries;
 
-  private Metadata() {
-    this.entries = Collections.emptyMap();
+  @JsonAnyGetter
+  public Map<String, String> getEntries() {
+    return entries;
   }
 
-  @Builder(toBuilder = true)
-  private Metadata(@Singular Map<String, String> entries) {
-    this.entries = entries;
+  @JsonCreator
+  private Metadata(Map<String, String> entries) {
+    this.entries = Collections.unmodifiableMap(entries);
   }
+
+  @Override
+  public String toString() {
+    return entries.toString();
+  }
+
+  public String get(String key) {
+    return this.entries.get(key);
+  }
+
+  public void put(String key, String value) {
+    this.entries.put(key, value);
+  }
+
+  public void remove(String key) {
+    this.entries.remove(key);
+  }
+
+
 
   @Transient
   public String getCorrelationId() {
