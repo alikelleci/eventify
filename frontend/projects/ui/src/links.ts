b7cf8e799c4ba5e3dac9b75ed1bc1b2dbde85beb
akelleci <a.kelleci@portbase.com>
2026-09-14 08:08:53 +0200

    wip website

--- frontend/projects/ui/src/links.ts
+++ frontend/projects/ui/src/links.ts
@@ -1,8 +1,8 @@
 /** The console's documentation, linked from the console and the Console page. */
-export const DOCS_URL = 'https://alikelleci.github.io/eventify/console/';
+export const DOCS_URL = 'https://alikelleci.github.io/eventify/docs/console/';
 
 /** The start of the Eventify documentation, linked from the website. */
-export const DOCS_HOME_URL = 'https://alikelleci.github.io/eventify/';
+export const DOCS_HOME_URL = 'https://alikelleci.github.io/eventify/docs/';
 
 /** The Eventify repository, linked from the website. */
 export const GITHUB_URL = 'https://github.com/alikelleci/eventify';
